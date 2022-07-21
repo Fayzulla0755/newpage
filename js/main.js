047297be562e5df328c9cecaa5eaa7e24d62c101
Fayzulla0755 <100923261+Fayzulla0755@users.noreply.github.com>
2022-07-21 23:11:26 +0500

    new==

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let body=document.querySelector("body");
+let navbar=document.querySelector(".navbar_asosiy");
+let dropdown1=document.querySelector(".dropdown");
+
+
+
+dropdown1.addEventListener('click',()=>{
+    document.qureySelector('.nav2').style.display="inline-block"
+})
+
+ function dropdown(){
+    
+        document.getElementById('dropdown1').classList.toggle("show")
+    
+ }
+
+
+
+
+
